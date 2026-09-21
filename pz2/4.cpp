e827d3956da2e58c35fda9153004900ccb9da419
Nikita <nikpetrov008@gmail.com>
2026-09-21 18:12:19 +0300

    pz2

---
+++ pz2/4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+
+int main() {
+    std::vector<int> arr = {1, 4, 2, 5, 0};
+    int summa_el_pr, n;
+    int summa_el_fact = 0;
+    n = arr.size();
+    summa_el_pr = (n * (n + 1)) / 2;
+    for (int i = 0; i < n; i++) {
+        summa_el_fact += arr[i];
+    }
+    std::cout << summa_el_pr - summa_el_fact << std::endl;
+    return 0;
+}
